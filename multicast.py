170da01185b78ba2afef1c3148e0a7beb324d200
blade- <stylemisty@gmail.com>
2015-04-05 15:52:03 -0400

    multicast

--- multicast.py
+++ multicast.py
@@ -1,5 +1,8 @@
 import setting
-import zerorpc
+import xmlrpclib 
+import SimpleXMLRPCServer
+
+#import zerorpc
 devNum = setting.devNum
 addli=[]
 
@@ -8,18 +11,19 @@ for i in setting.localadd:
     addli.append(setting.localadd[i])
 #print addli
 
-addli = ["tcp://0.0.0.0:12000","tcp://0.0.0.0:12001","tcp://0.0.0.0:12002"]
+#addli = ["tcp://0.0.0.0:12000","tcp://0.0.0.0:12001","tcp://0.0.0.0:12002"]
 
 def multicast(selfadd,vector):
     '''multi-cast to all other users in the group, except for self address'''
     
     for add in addli:
         if add != selfadd:
-            c = zerorpc.Client()
-            c.connect(add)
+            c = xmlrpclib.ServerProxy("http://"+add[0]+":"+str(add[1]))
+            #c = zerorpc.Client()
+            #c.connect(add)
             c.update_vector_clock(vector)
-            c.close()
+            #c.close()
 
     
 
-multicast("tcp://0.0.0.0:12002",[5,1,2,3,4,5])
+#multicast("tcp://0.0.0.0:12002",[5,1,2,3,4,5])
